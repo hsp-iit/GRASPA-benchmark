779f4024fc2d66542e2b43eafa7e0de247f22579
Giulia Vezzani <vzzgli@gmail.com>
2019-06-12 10:21:43 +0200

    also grasping binary information are parsed

--- src/scores_evalution.py
+++ src/scores_evalution.py
@@ -1,12 +1,18 @@
 import numpy as np
 import xml.etree.ElementTree as ET
 from scipy.spatial.transform import Rotation as R
+import os, re, sys
+from os import listdir
+from os.path import isfile, join
+
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--file_reachability_test', action='store', dest="reached_poses_file",
                     help='file containing the reached poses during reachability tests')
-parser.add_argument('--folder_grasping_test', action='store', dest="grasping_folder",
+parser.add_argument('--grasps_folder', action='store', dest="grasping_folder",
+                    help='folder containing the xml with the graspings tests for one layout')
+parser.add_argument('--scenes_folder', action='store', dest="scenes_folder", default='data/scenes/grasping/3D_scenes/',
                     help='folder containing the xml with the graspings tests for one layout')
 
 # Params useful for xml parsing
@@ -18,12 +24,8 @@ acceptable_keys = ['Reachable_frame00', 'Reachable_frame01', 'Reachable_frame02'
 testing_layout = None
 # Scores computed (TODO)
 # ------- Reachability -------
-# s0_1 = Reachability score, region 1
-s0_1 = 0.0
-# s0_2 = Reachability score, region 2
-s0_2 = 0.0
-# s0_3 = Reachability score, region 3
-s0_3 = 0.0
+# s0 = Reachability score, for region 1,2 and 3
+s0 = {}
 # ------- Cam Calibration -------
 # s1_1 = Reachability score, region 1
 # s1_2 = Reachability score, region 2
@@ -46,6 +48,43 @@ s5 = {}
 region_1 = np.array([[0.0, 0.0], [0.0, 123.33], [-544, 0.0], [-544, 123.33]])
 region_2 = np.array([[-0.0, 123.33], [0.0, 246.66], [-544, 123.33], [-544, 246.66]])
 region_3 = np.array([[-0.0, 246.66], [-0.0, 370], [-544, 246.66], [-544, 370]])
+
+def not_consistent(file, acceptable_object_names):
+
+    tree = ET.parse(file)
+    root = tree.getroot()
+
+    file_name = os.path.splitext(os.path.basename(file))[0]
+    file_name = file_name[:-6]
+
+    if file_name in acceptable_object_names[root[2].attrib['name']]:
+        return False
+    else:
+        return True
+
+def parse_grasping_files(files, s3, s4, s5):
+
+    for file in files:
+        file_name = os.path.splitext(os.path.basename(file))[0]
+        file_name = file_name[:-6]
+
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        s3[file_name] = float(root[3].attrib['quality'])
+        s4[file_name] = float(root[4].attrib['quality'])
+        s5[file_name] = float(root[5].attrib['quality'])
+
+
+def parse_scenes_files(dict_store, root):
+
+    objects_list = []
+    # Parse scene files
+    for pose in root.iter('ManipulationObject'):
+        objects_list.append(pose.attrib['name'])
+
+    dict_store[root.attrib['name']] = objects_list
+
 
 def parse_reachability_files(dict_store, root):
 
@@ -143,24 +182,44 @@ def compute_reachability_score(args):
     s0_1 = s0_1 / n_poses_1
     s0_2 = s0_2 / n_poses_2
     s0_3 = s0_3 / n_poses_3
+    s0['s0_1'] = s0_1
+    s0['s0_2'] = s0_2
+    s0['s0_3'] = s0_3
 
     print('Reachability scores:')
-    print( "|| s0_1    :", s0_1)
-    print( "|| s0_2    :", s0_2)
-    print( "|| s0_3    :", s0_3)
+    print( "|| s0_1    :", s0['s0_1'])
+    print( "|| s0_2    :", s0['s0_2'])
+    print( "|| s0_3    :", s0['s0_3'])
 
 
 def read_scores(args):
 
-    # TODO For each file in the folder_grasping_test
+    # Parse the scenes to load object labels
+    scenes_files = [f for f in listdir(args.scenes_folder) if isfile(join(args.scenes_folder,f))]
+
+    # acceptable_object_names lists all the objects name included in a scene
+    acceptable_object_names = {}
+    for file in scenes_files:
+        tree = ET.parse(join(args.scenes_folder,file))
+        root = tree.getroot()
+        parse_scenes_files(acceptable_object_names,root)
+
+
+    # Collect file in the folder_grasping_test
+    grasp_files = [join(args.grasping_folder,f) for f in listdir(args.grasping_folder) if isfile(join(args.grasping_folder,f))]
+
+    # Check consistency between object and layout
+    for file in grasp_files:
+        if (not_consistent(file, acceptable_object_names)):
+            sys.exit("Inconsistency between provided object names and benchmark layout!")
+
     # Read Binary scores and save in s2 as a dictionary, with tag the names
     # of the objects as defined in the xmls of the benchmark
-
     # Read Graspable scores and save in s3 as a dictionary, with tag the names
     # of the objects as defined in the xmls of the benchmark
-
     # Read Stability scores and save in s4 as a dictionary, with tag the names
     # of the objects as defined in the xmls of the benchmark
+    parse_grasping_files(grasp_files, s3, s4, s5)
 
 
 if __name__ == '__main__':
@@ -172,5 +231,7 @@ if __name__ == '__main__':
     # Compute grasp Quality
     # TODO To launch fabrizio's code
 
-    # Read scores s2_i, s3_i, s4_i from user provided files
+    # Read scores s3, s4, s5 from user provided files
     read_scores(parser.parse_args())
+    import IPython
+    IPython.embed()
