ecaf3e252d48124c01977266fb6ce571632f6423
Giulia Vezzani <vzzgli@gmail.com>
2019-08-28 13:58:53 +0200

    fixed path in script launcher

--- src/script_launcher.sh
+++ src/script_launcher.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-REACHED_POSES_FOLDER=/home/gvezzani/giulia_code/GRASPA-test/experiment_data/right_arm/reaching_test/
-FILE_CAMERA_CALIBRATION=/home/gvezzani/giulia_code/GRASPA-test/experiment_data/right_arm/camera_calibration/cam_calibration_test_output.xml
-GRASPS_FOLDER=/home/gvezzani/giulia_code/GRASPA-test/experiment_data/right_arm/grasps_data/layout_0
+REACHED_POSES_FOLDER=$GRASPA_TEST_DIR/reaching_test/
+FILE_CAMERA_CALIBRATION=$GRASPA_TEST_DIR/camera_calibration/cam_calibration_test_output.xml
+GRASPS_FOLDER=$GRASPA_TEST_DIR/grasps_data/layout_0
 LAYOUT_NAME=Benchmark_Layout_0
 MODALITY=isolation
 THRES_POS_REACH=0.02
