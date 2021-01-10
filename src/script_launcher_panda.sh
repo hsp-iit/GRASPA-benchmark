d6b1c48dc6168d35a25c5f3eb9d5a681b2ca2883
fbottarel <fabrizio.bottarel@iit.it>
2021-01-10 22:56:38 +0000

    added variable for layout number to evaluation script

---
+++ src/script_launcher_panda.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GRASPA_TEST_DIR=/workspace/GRASPA-test/experiment_data/panda-dexnet
+LAYOUT=2
+
+
+REACHED_POSES_FOLDER=$GRASPA_TEST_DIR/reaching_test/
+FILE_CAMERA_CALIBRATION=$GRASPA_TEST_DIR/camera_calibration/cam_calibration_test_output.xml
+GRASPS_FOLDER=$GRASPA_TEST_DIR/grasps_data/layout_${LAYOUT}
+LAYOUT_NAME=Benchmark_Layout_${LAYOUT}
+MODALITY=isolation
+THRES_POS_REACH=0.02
+THRES_ORIE_REACH=0.5
+THRES_REACH=0.5
+THRES_POS_CAM=0.045
+THRES_ORIE_CAM=0.8
+THRES_CAM=0.5
+
+
+## To launch pose quality evaluation
+compute-grasp-quality --scene $GRASPA_DIR/data/scenes/grasping/3D_scenes/layout_${LAYOUT}.xml --graspsdir $GRASPA_TEST_DIR/grasps_data --datapath $GRASPA_DIR --robot /workspace/GRASPA-test/experiment_data/panda/panda_gripper/panda_gripper_8_contacts.xml
+
+
+## To launch the script to compute all the scores
+python3 src/scores_evaluation.py --reached_poses_folder="${REACHED_POSES_FOLDER}" --file_camera_calibration_test="${FILE_CAMERA_CALIBRATION}" --grasps_folder="${GRASPS_FOLDER}" --layout="${LAYOUT_NAME}" --modality="${MODALITY}" --threshold_pos=${THRES_POS_REACH} --threshold_orie=${THRES_ORIE_REACH} --reaching_threshold=${THRES_REACH} --threshold_camera_pos=${THRES_POS_CAM} --threshold_camera_orie=${THRES_ORIE_CAM} --camera_threshold=${THRES_CAM}
